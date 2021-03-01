791c6d3c9284b2919e05d78625866d18afd50812
Snuuson <sebastian.heinz1989@gmail.com>
2021-03-01 18:22:53 +0100

    Implemented CameraController
    Basic camera movement (Zoom/Pan/Rotate)

--- Assets/Scripts/CameraControll.cs
+++ Assets/Scripts/CameraControll.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class CameraControll : MonoBehaviour
 {
 
-    [SerializeField] float zoomSpeedY = 3f;
-    [SerializeField] float zoomSpeedZ = 3f;
+    [SerializeField] float zoomSpeed = 50f;
 
-    [SerializeField] float scrollSpeedHorizontal = 1f;
-    [SerializeField] float scrollSpeedVertical = 1f;
+    [SerializeField] float panSpeed = 50f;
+
+    
+    [SerializeField] float rotationSpeed = 3.5f;
+    [SerializeField] float sidePanPercent = 0.05f;
+    bool isRotating = false;
+    Vector3 rotateStartMousePosition = new Vector3();
+
+    Quaternion cameraStartRotation = new Quaternion();
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +27,140 @@ public class CameraControll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseWheel = Input.GetAxis("Mouse ScrollWheel") * -1;
-        transform.position += new Vector3(0f,mouseWheel * zoomSpeedY * Time.deltaTime,mouseWheel * zoomSpeedZ * Time.deltaTime);
+        
+        
+        Pan();
+        Zoom();
+        Rotate();
+    }
 
-        transform.position += new Vector3(Input.GetAxis("Horizontal") * scrollSpeedHorizontal * Time.deltaTime,0f,Input.GetAxis("Vertical") * scrollSpeedVertical * Time.deltaTime);
+    private void Zoom()
+    {
+        float mouseWheel = Input.GetAxis("Mouse ScrollWheel") ;
+        transform.Translate(transform.forward * mouseWheel * zoomSpeed * 10 * Time.deltaTime,Space.World);
+    }
+    
+    private void HorizontalKeyboardPan()
+    {
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            Vector3 right = transform.right;
+            right.y = 0;
+            right.Normalize();
+            PanInDirection(right);
+        }
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            Vector3 left = transform.right * -1;
+            left.y = 0;
+            left.Normalize();
+            PanInDirection(left);
+        }
+    }
+    
+    private void verticalKeyboardPan()
+    {
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            Vector3 up = transform.up;
+            up.y = 0;
+            up.Normalize();
+            PanInDirection(up);
+        }
+        if(Input.GetAxis("Vertical") < 0)
+        {
+            Vector3 down = transform.up * -1;
+            down.y = 0;
+            down.Normalize();
+            PanInDirection(down);
+        }
+    }
+
+    private void Pan()
+    {
+        KeyboardPan();
+        if(!isRotating)
+        {
+            MousePan();
+        }
+        
+    }
+    private void KeyboardPan()
+    {
+        HorizontalKeyboardPan();
+        verticalKeyboardPan();
+    }
+    private void MousePan()
+    {
+        HorizontalMousePan();
+        VerticalMousePan();
+    }
+
+    private void PanInDirection(Vector3 direction)
+    {
+        transform.Translate(direction * Time.deltaTime * panSpeed,Space.World);
+    }
+
+    private void HorizontalMousePan()
+    {
+        Vector3 right = transform.right;
+        right.y = 0;
+        right.Normalize();
+        Vector3 left = right * - 1;
+        
+        
+         if ( Input.mousePosition.x >= Screen.width * (1 - sidePanPercent))
+         {
+            PanInDirection(right);
+         }
+         if ( Input.mousePosition.x <= Screen.width * (0 + sidePanPercent))
+         {
+            PanInDirection(left);
+         }
+    }
+
+    private void VerticalMousePan()
+    {
+        Vector3 forward = transform.forward;
+        forward.y = 0;
+        forward.Normalize();
+        Vector3 back = forward * -1;
+
+        if ( Input.mousePosition.y >= Screen.height * (1 - sidePanPercent))
+         {
+             PanInDirection(forward);
+         }
+         if ( Input.mousePosition.y <= Screen.height * (0 + sidePanPercent))
+         {
+             PanInDirection(back);
+         }
+    }
+
+    private void Rotate()
+    {
+        if(Input.GetMouseButtonDown(2) && !isRotating)
+        {
+            
+            isRotating = true;
+            rotateStartMousePosition = Input.mousePosition;
+            cameraStartRotation = transform.rotation;
+        }
+
+        if(Input.GetMouseButtonUp(2))
+        {
+            isRotating = false;
+        }
+
+        if(isRotating)
+        {
+
+            
+            transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y") * rotationSpeed, Input.GetAxis("Mouse X") * rotationSpeed, 0));
+            float X = transform.rotation.eulerAngles.x;
+            float Y = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(X, Y, 0);
+        }
+
+
     }
 }
