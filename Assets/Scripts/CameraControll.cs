747f4bbe1b5da680372133f12a73e32a500fd875
Snuuson <sebastian.heinz1989@gmail.com>
2021-03-01 14:10:57 +0100

    Camera movement (Horizontal/Vertical)

--- Assets/Scripts/CameraControll.cs
+++ Assets/Scripts/CameraControll.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +7,9 @@ public class CameraControll : MonoBehaviour
 
     [SerializeField] float zoomSpeedY = 3f;
     [SerializeField] float zoomSpeedZ = 3f;
+
+    [SerializeField] float scrollSpeedHorizontal = 1f;
+    [SerializeField] float scrollSpeedVertical = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +21,7 @@ public class CameraControll : MonoBehaviour
     {
         float mouseWheel = Input.GetAxis("Mouse ScrollWheel") * -1;
         transform.position += new Vector3(0f,mouseWheel * zoomSpeedY * Time.deltaTime,mouseWheel * zoomSpeedZ * Time.deltaTime);
+
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * scrollSpeedHorizontal * Time.deltaTime,0f,Input.GetAxis("Vertical") * scrollSpeedVertical * Time.deltaTime);
     }
 }
