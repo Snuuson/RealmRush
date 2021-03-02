5ab755ce5ab0fbe29df83a6afa5ff794fd4cdb9a
Snuuson <sebastian.heinz1989@gmail.com>
2021-03-02 01:07:44 +0100

    Added Basic Grid classes

---
+++ Assets/Grid/GridController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    
+    Dictionary<Vector2Int,GridTile> grid = new Dictionary<Vector2Int, GridTile>();
+    int gridSize;
+    float tileSize;
+
+    private void createGrid()
+    {
+
+    }
+
+
+    public Vector2Int GetCoordinatesFromPosition(Vector3 position)
+    {
+        Vector2Int coordinates = new Vector2Int();
+        coordinates.x = Mathf.RoundToInt(position.x / tileSize);
+        coordinates.y = Mathf.RoundToInt(position.z / tileSize);
+        return coordinates;
+    }
+
+    public Vector3 GetPositionFromCoordinates(Vector2Int coordinates)
+    {
+        Vector3 position = new Vector3();
+        position.x = coordinates.x * tileSize;
+        position.z = coordinates.y * tileSize;
+        return position;
+    }
+    
+}
---
+++ Assets/Grid/GridTile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTile : MonoBehaviour
+{
+    Vector2Int coordinates;
+
+    float width;
+    float height;
+    Vector3 position;
+    Vector2 center;
+
+}
